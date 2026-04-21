eea035949ad514ea83ed157148a1bcedaccba888
nitink073 <khatriniiitin358@gmail.com>
2026-04-21 11:22:12 +0530

    first commit

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  database: process.env.MYDATABASE,
+  port: process.env.PORT,
 //   waitforconnections:true,
 //   connectionlimit:10,
 //   connectionTimeout:10000
