3da11690099b1e8e8abfd171fd3b92281fe5ebb1
nitink073 <khatriniiitin358@gmail.com>
2026-04-21 11:18:44 +0530

    first commit

--- db.js
+++ db.js
@@ -7,9 +7,9 @@ const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  waitforconnections:true,
-  connectionlimit:10,
-  connectionTimeout:10000
+//   waitforconnections:true,
+//   connectionlimit:10,
+//   connectionTimeout:10000
 })
 
 module.exports = pool;
